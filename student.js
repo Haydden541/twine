d03f75fbbf0b9ba67116599ad9b192b229f136aa
Haydden541 <1365213202@qq.com>
2026-03-08 23:10:12 +0800

    Create student.js

---
+++ student.js
@@ -0,0 +1,7 @@
+// 以后新增或修改学生，只需改动这个文件，不需要碰 HTML
+const studentData = {
+    "张三": { major: "游戏", country: "美国" },
+    "李四": { major: "交互设计", country: "英国" },
+    "王五": { major: "动画", country: "加拿大" },
+    "赵六": { major: "插画", country: "法国" }
+};
